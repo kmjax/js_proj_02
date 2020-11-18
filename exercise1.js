d650defdae99358acc6f1f79be1f251e1b16934b
Kelsey McClanahan <jaxcode@kmcode.com>
2020-11-18 15:40:36 -0500

    Completed exercise 1 of 4

---
+++ exercise1.js
@@ -0,0 +1,9 @@
+// ===============================================
+// Log numbers between 50 and 100 to the console.
+// Jaxcode Day 9 Homework Assignment (1 of 4)
+//
+// Author: Kelsey McClanahan
+// Date:   11/18/2020
+// ===============================================
+
+for (let i = 51; i < 100; i++) console.log(i);
